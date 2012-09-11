2820468b17c5f3f90e9853570d3d38b17042acf9
Kerry Jiang <kerry-jiang@hotmail.com>
2012-09-11 23:58:18 +0800

    added windows domain membership class

---
+++ FtpServer/Membership/WindowsDomainMembership.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.DirectoryServices.AccountManagement;
+
+namespace SuperSocket.Ftp.FtpService.Membership
+{
+    public class WindowsDomainMembership
+    {
+        public bool ValidateUser(string username, string password)
+        {
+            using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, "YOURDOMAIN"))
+            {
+                // validate the credentials
+                return pc.ValidateCredentials(username, password);
+            }
+        }
+    }
+}
